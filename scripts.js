36596927492b64c82abff621ee34f9aba700042c
Jacob Godwin <jpgodwin@ufl.edu>
2022-06-27 14:47:18 -0400

    implement marker logic

--- scripts.js
+++ scripts.js
@@ -1,11 +1,39 @@
 const gameBoard = (() => {
   let board = document.getElementById("gameBoard");
-  const squares = 9;
+  let allMarkers = Array.from(document.getElementsByClassName("marker"));
 
-  for (let i = 0; i < squares; i++) {
-    let gameSquare = document.createElement("div");
-    gameSquare.className = "gameSquare";
-    gameSquare.id = `${"square" + [i + 1]}`;
-    board.appendChild(gameSquare);
-  }
+  let playSelection = (e) => {
+    if (e.target.innerHTML === "X") {
+      removeClass(e.target.parentElement);
+      drawMarker(e.target.parentElement, e.target.innerHTML);
+      checkBoard();
+    } else if (e.target.innerHTML === "O") {
+      removeClass(e.target.parentElement);
+      drawMarker(e.target.parentElement, e.target.innerHTML);
+      checkBoard();
+    }
+  };
+
+  allMarkers.forEach((element) =>
+    element.addEventListener("click", playSelection)
+  );
+
+  let removeClass = (e) => {
+    e.classList.remove("inactive");
+    e.classList.add("active");
+  };
+
+  let drawMarker = (e, value) => {
+    e.innerHTML = value;
+  };
+
+  let checkBoard = () => {
+    let activeSquares = Array.from(document.getElementsByClassName("active"));
+    console.log(activeSquares);
+  };
 })();
+
+const Player = (name, marker) => {
+  this.name = name;
+  this.marker = marker;
+};
